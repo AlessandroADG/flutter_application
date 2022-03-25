f3c5e14ec1811bc6791e206b47fe87d6c0e1aaa7
delgale <delgalessandro@gmail.com>
2022-03-25 15:50:40 +0100

    delete useless branch and app

--- theme_change/lib/list_app/readme.txt
+++
@@ -1,4 +0,0 @@
-This is an app where, you can see a list tile that shows the activities that you have to do;
-it's pretty easy.
-in the future, this will be a complex application.
-AADG.
--- theme_change/lib/main.dart
+++ theme_change/lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:theme_change/list_app/screen/list_screen.dart';
+import 'package:theme_change/screen/theme_app.dart';
 
 void main() {
-  runApp(const ListScreen());
+  runApp(const ThemeApp());
 }
 
